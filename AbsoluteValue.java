5d33d29d91bda2c1b46a8f0d023934e633d5317f
Alexini007 <100991916+Alexini007@users.noreply.github.com>
2022-03-15 13:36:01 +0200

    Delete AbsoluteValue.java

--- AbsoluteValue.java
+++
@@ -1,7 +0,0 @@
-package mypac;
-
-public class AbsoluteValue {
-    public static void main (String[]args) {
-        System.out.println(Math.abs(-147) + "\t" + Math.abs(15) + "\t" + Math.abs(0) + "\t" + Math.abs(18));
-    }
-}
