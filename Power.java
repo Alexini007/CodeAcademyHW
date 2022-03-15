683774f49c76493440008d91b4af4bb859d30c30
Alexini007 <100991916+Alexini007@users.noreply.github.com>
2022-03-15 13:29:47 +0200

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,9 @@
+package mypac;
+
+public class Power {
+    public static void main (String[]args) {
+        System.out.println((int)Math.pow(-20,2) + "   " + (int)Math.pow(92,2) + "   " + (int)Math.pow(638,2)
+                + "   " + (int)Math.pow(73,2) + "   " + (int)Math.pow(200,2) + "   "
+                + (int)Math.pow(-22,2));
+    }
+}
